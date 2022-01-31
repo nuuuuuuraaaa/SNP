03fefa2bb899b7e71c07ab427e353406d1300c25
anastasiababoshina <baboshina_nastya@mail.ru>
2022-01-31 21:14:11 +0300

    Updates

---
+++ 811e56b9354cb52064c8.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(){let e=window.scrollY;const t=document.getElementById("header");window.addEventListener("scroll",(function(){e=window.scrollY,e>450&&t.classList.add("header-active"),e<450&&t.classList.remove("header-active")}))}()})();
